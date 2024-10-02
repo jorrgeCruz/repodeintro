a149b3266a528f6e4403230b3a9b1eeae766cd0f
Jorrge <wearevenom0777@gmail.com>
2024-10-02 13:48:29 -0600

    modifique linea 8 y corregi errores

--- Ejemplo.java
+++ Ejemplo.java
@@ -1,11 +1,11 @@
 public class Ejemplo{
 
 	public static void main(String args[]){
-		System.oot.println("Hola mundo")
+		
 (
     (
         (
-            
+           System.out.println("Hola mundo"); 
         )
     )
 )
