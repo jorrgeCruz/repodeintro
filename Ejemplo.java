c9fa9e85b8116898e06c9941135daf6ecdfd3c3a
Jorrge <wearevenom0777@gmail.com>
2024-10-03 13:31:04 -0600

    Cambios realizados desde version de prueba pero en main

--- Ejemplo.java
+++ Ejemplo.java
@@ -1,11 +1,11 @@
 public class Ejemplo{
 
-	public static void main(String args[]){
-		
+	public static void main(String args[]){	
 (
     (
         (
            System.out.println("Hola mundo"); 
+           float varFlotante = 3.14159
         )
     )
 )
