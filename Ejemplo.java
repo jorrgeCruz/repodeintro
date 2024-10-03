e58fb95e1090827a3e12585690eafd59d735a535
Jorrge <wearevenom0777@gmail.com>
2024-10-03 13:23:14 -0600

    Cambios realizados desde la rama copia

--- Ejemplo.java
+++ Ejemplo.java
@@ -1,11 +1,12 @@
 public class Ejemplo{
 
-	public static void main(String args[]){
-		
+	public static void main(String args[]){	
 (
     (
         (
            System.out.println("Hola mundo"); 
+           // esta es la version desde la rama copia
+           int valorEntero = 10;
         )
     )
 )
